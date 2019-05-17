4e41d080fe553c3e75099b197e22b1d392c4795a
suntao <358777330@qq.com>
2019-05-17 21:41:52 +0800

    baseUrl, login api

--- vue.config.js
+++ vue.config.js
@@ -37,14 +37,17 @@ module.exports = {
       // change xxx-api/login => mock/login
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:${port}/mock`,
+        // target: `http://localhost:${port}/mock`,
+        target: `http://129.211.23.151:8088`,
+        // target: `http://127.0.0.1:8000`,
         changeOrigin: true,
+        ws: false,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
         }
       }
     },
-    after: require('./mock/mock-server.js')
+    // after: require('./mock/mock-server.js')
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
